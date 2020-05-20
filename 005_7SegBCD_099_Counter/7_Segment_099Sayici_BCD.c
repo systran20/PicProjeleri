5b1d2dec8c216b0d31f7ba551fa7e71f157ebcf7
Systran <47870746+systran20@users.noreply.github.com>
2020-05-20 05:40:39 +0300

    Add files via upload

---
+++ 005_7SegBCD_099_Counter/7_Segment_099Sayici_BCD.c
@@ -0,0 +1,23 @@
+#include <16f877.h>  //derleyici kütüphanesini ekle
+#fuses XT, NOWDT, NOPROTECT, NOBROWNOUT, NOLVP, NOPUT, NOWRT, NODEBUG, NOCPD //kullanýlmayan bazý özelliklerini kapat
+#use delay(clock=4M) // osilatör hýzý 4 Mhz olarak ayarla
+
+int8 i=0;
+int8 iBCD=0;
+void main() {
+   set_tris_c(0b00000000); // C portunu OUTPUT yapar
+   delay_ms(50);
+   output_c(0x00);      // C portunu 0V yapar
+   delay_ms(50);
+   
+   //loop bloðu sürekli tekrar eder
+   while(1) {
+      iBCD = ((i/10) + ((i%10)<<4));
+      output_c(iBCD );
+      delay_ms(100);
+      i++;
+      if (i>99) {i=0;}
+      
+   }   
+}
+
